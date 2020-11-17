2e08ac42b9f836b5854e8fe3a3010015540e2435
Chu, Ryan <ryan.chu@nordicsemi.no>
2020-11-17 20:21:19 +0100

    test.py: Support connect_to_emu_with_snr
    python3 test.py 192.168.0.14 19050 683585795

--- test.py
+++ test.py
@@ -11,7 +11,11 @@ port = sys.argv[2]
 api.select_ip(addr.encode(), int(port))
 
 print(api.enum_emu_snr())
-api.connect_to_emu_without_snr()
+if len(sys.argv) > 3:
+    api.connect_to_emu_with_snr(int(sys.argv[3]))
+else:
+    api.connect_to_emu_without_snr()
+api.pin_reset()
 api.erase_all()
 #api.write_u32(ADDRESS, DATA, IS_FLASH)
 api.disconnect_from_emu()
